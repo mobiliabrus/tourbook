3f6b430c232673669f44d1ea8af226c530a68c78
chiaweilee <97830321+mobiliabrus@users.noreply.github.com>
2022-07-24 13:25:43 +0800

    style: fix

--- src/components/a-secret.js
+++ src/components/a-secret.js
@@ -1,12 +1,10 @@
 import CryptoJS from "crypto-js";
 
-const template = `<span v-if="!visible" @click="decrypt" style="display: inline-block;
+const template = `<span v-if="!visible" @click="decrypt" style="display: inline;
 background-color: #333;
 color: transparent;
 padding: 0 8px;
 user-select: none;
-font-size: 0;
-width: 100%;
 height: 18px;
 line-height: 18px;
 word-break: break-all;
@@ -34,9 +32,15 @@ export default {
       fetch(`./confidential/${this.name}.md`)
         .then((res) => res.text())
         .then((rawContent) => {
+          const keylength = 16;
+          const keyorigin = this.secretKey.split("");
+          const key16 = keyorigin.length < 16 ? [...keyorigin, ...Array.from(new Array(keylength - keyorigin.length)).map(() => "0")].join("") : key16;
+          const keyutf = CryptoJS.enc.Utf8.parse(key16);
+          const iv = { iv: CryptoJS.enc.Base64.parse(key16) };
+
           this.rawContent = rawContent;
-          const bytes = CryptoJS.AES.decrypt(rawContent, this.secretKey);
-          const content = bytes.toString(CryptoJS.enc.Utf8);
+          const raw = CryptoJS.AES.decrypt({ ciphertext: CryptoJS.enc.Base64.parse(rawContent) }, keyutf, iv);
+          const content = CryptoJS.enc.Utf8.stringify(raw);
           this.content = content;
         });
     }
