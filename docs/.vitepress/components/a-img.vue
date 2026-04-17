3f82068f5501f19ce7998eec64b71a39d8e4432e
plus <jawoo@qq.com>
2026-04-17 08:31:35 +0800

    feat: exif

--- docs/.vitepress/components/a-img.vue
+++ docs/.vitepress/components/a-img.vue
@@ -29,6 +29,7 @@ const src = ref<string | undefined>(undefined)
 const srcMin = ref<string | undefined>(undefined)
 const scale = ref<number | undefined>(undefined)
 const loading = ref(false)
+const exifInfo = ref<any>(null)
 const visible = computed(() => !props.dir?.includes('privacy') || !!secretKey.value)
 
 const onLoad = () => {
@@ -43,6 +44,18 @@ const readExif = async (data: ArrayBuffer | SharedArrayBuffer) => {
     const exifData = await ExifReader.load(data)
     if (exifData) {
       console.log('EXIF data:', exifData)
+      
+      // Extract camera info
+      exifInfo.value = {
+        make: exifData.Make?.description || '',
+        model: exifData.Model?.description || '',
+        focalLength: exifData.FocalLength?.description || '',
+        focalLength35mm: exifData.FocalLengthIn35mmFilm?.description || exifData.FocalLength35efl?.description || '',
+        fNumber: exifData.FNumber?.description || '',
+        exposureTime: exifData.ExposureTime?.description || ''
+      }
+      
+      console.log('Camera info:', exifInfo.value)
     }
   } catch (error) {
     console.warn('Failed to parse EXIF data:', error)
@@ -171,12 +184,25 @@ const load = async (suffix = '', t: 'src' | 'srcMin' = 'src') => {
     <a-placeholder v-if="visible && (!src && !srcMin)"></a-placeholder>
     <a-modal v-if="!loading || (src || srcMin)" :scale="scale" @popover="handlePopoverOpen">
       <template #action>
-        <div style="display: flex; justify-content: space-between;">
+        <div style="display: flex; justify-content: space-between; align-items: center;">
           <div class="a-img-lefaction">
             <button type="button" style="padding:4px 5px;" v-if="!src" @click="loadHD" :loading="loading">
               <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M128 128h768a42.666667 42.666667 0 0 1 42.666667 42.666667v682.666666a42.666667 42.666667 0 0 1-42.666667 42.666667H128a42.666667 42.666667 0 0 1-42.666667-42.666667V170.666667a42.666667 42.666667 0 0 1 42.666667-42.666667z m192 352V384H256v256h64v-96h85.333333V640H469.333333V384H405.333333v96h-85.333333z m298.666667-32H682.666667a21.333333 21.333333 0 0 1 21.333333 21.333333v85.333334a21.333333 21.333333 0 0 1-21.333333 21.333333h-64v-128zM554.666667 384v256h128a85.333333 85.333333 0 0 0 85.333333-85.333333v-85.333334a85.333333 85.333333 0 0 0-85.333333-85.333333h-128z" fill="#fff"></path></svg>
             </button>
           </div>
+          
+          <!-- Camera info display -->
+          <div v-if="exifInfo && (exifInfo.model || exifInfo.focalLength)" class="a-img-exif-info">
+            <span v-if="exifInfo.make || exifInfo.model" class="exif-item">
+              {{ [exifInfo.make, exifInfo.model].filter(Boolean).join(' ') }}
+            </span>
+            <span v-if="exifInfo.focalLength" class="exif-item">
+              {{ exifInfo.focalLength }}{{ exifInfo.focalLength35mm ? ` (${exifInfo.focalLength35mm})` : '' }}
+            </span>
+            <span v-if="exifInfo.fNumber" class="exif-item">{{ exifInfo.fNumber }}</span>
+            <span v-if="exifInfo.exposureTime" class="exif-item">{{ exifInfo.exposureTime }}s</span>
+          </div>
+          
           <div class="a-img-righaction">
             <button type="button" style="padding:4px 5px;" @click="scaleIn">
               <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M149.333333 394.666667c17.066667 0 32-14.933333 32-32v-136.533334l187.733334 187.733334c6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333c12.8-12.8 12.8-32 0-44.8l-187.733333-187.733334H362.666667c17.066667 0 32-14.933333 32-32s-14.933333-32-32-32H149.333333c-4.266667 0-8.533333 0-10.666666 2.133334-8.533333 4.266667-14.933333 10.666667-19.2 17.066666-2.133333 4.266667-2.133333 8.533333-2.133334 12.8v213.333334c0 17.066667 14.933333 32 32 32zM874.666667 629.333333c-17.066667 0-32 14.933333-32 32v136.533334L642.133333 597.333333c-12.8-12.8-32-12.8-44.8 0s-12.8 32 0 44.8l200.533334 200.533334H661.333333c-17.066667 0-32 14.933333-32 32s14.933333 32 32 32h213.333334c4.266667 0 8.533333 0 10.666666-2.133334 8.533333-4.266667 14.933333-8.533333 17.066667-17.066666 2.133333-4.266667 2.133333-8.533333 2.133333-10.666667V661.333333c2.133333-17.066667-12.8-32-29.866666-32zM381.866667 595.2l-200.533334 200.533333V661.333333c0-17.066667-14.933333-32-32-32s-32 14.933333-32 32v213.333334c0 4.266667 0 8.533333 2.133334 10.666666 4.266667 8.533333 8.533333 14.933333 17.066666 17.066667 4.266667 2.133333 8.533333 2.133333 10.666667 2.133333h213.333333c17.066667 0 32-14.933333 32-32s-14.933333-32-32-32h-136.533333l200.533333-200.533333c12.8-12.8 12.8-32 0-44.8s-29.866667-10.666667-42.666666 0zM904.533333 138.666667c0-2.133333 0-2.133333 0 0-4.266667-8.533333-10.666667-14.933333-17.066666-17.066667-4.266667-2.133333-8.533333-2.133333-10.666667-2.133333H661.333333c-17.066667 0-32 14.933333-32 32s14.933333 32 32 32h136.533334l-187.733334 187.733333c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 14.933333 8.533333 23.466667 8.533333s17.066667-2.133333 23.466667-8.533333l187.733333-187.733333V362.666667c0 17.066667 14.933333 32 32 32s32-14.933333 32-32V149.333333c-2.133333-4.266667-2.133333-8.533333-4.266667-10.666666z" fill="#fff"></path></svg>
@@ -218,6 +244,27 @@ const load = async (suffix = '', t: 'src' | 'srcMin' = 'src') => {
   width: 50%;
   text-align: right;
   float: right;
+}
+
+.a-img-exif-info {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  color: #fff;
+  font-size: 13px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+  white-space: nowrap;
+  padding: 0 8px;
+}
+
+.exif-item {
+  opacity: 0.9;
+}
+
+.exif-item:not(:last-child)::after {
+  content: '·';
+  margin-left: 12px;
+  opacity: 0.5;
 }
 
 </style>
